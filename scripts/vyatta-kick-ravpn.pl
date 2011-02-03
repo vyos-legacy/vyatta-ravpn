193efac6fbaf73ad17f40753861778cd03651524
John Southworth <john.southworth@vyatta.com>
2011-02-03 13:20:05 -0600

    Fix bug 4079: added ability to clear a specific interface; included some node.def files i forgot to commit for bugs 5313, and 5547

--- scripts/vyatta-kick-ravpn.pl
+++ scripts/vyatta-kick-ravpn.pl
@@ -3,6 +3,7 @@
 use strict;
 
 my $username = shift;
+my $clearif  = shift;
 
 my $SESSION_PATH = '/opt/vyatta/etc/ravpn/sessions';
 
@@ -27,13 +28,25 @@ my @pids = ();
 foreach my $ses (@sessions) {
   $ses =~ /^(.+)\@([^@]+)$/;
   my ($u, $intf) = ($1, $2);
-  if ($u eq $username) {
-    open(SFILE, "$SESSION_PATH/$ses") or next;
-    my $pid = <SFILE>;
-    close(SFILE);
-    chomp($pid);
-    next if (!($pid =~ /^\d+$/));
-    push @pids, $pid;
+  if (defined $clearif){
+    if ($intf eq $username) {
+      open(SFILE, "$SESSION_PATH/$ses") or next;
+      my $pid = <SFILE>;
+      close(SFILE);
+      chomp($pid);
+      next if (!($pid =~ /^\d+$/));
+      push @pids, $pid;
+    }
+  }
+  else {
+    if ($u eq $username) {
+      open(SFILE, "$SESSION_PATH/$ses") or next;
+      my $pid = <SFILE>;
+      close(SFILE);
+      chomp($pid);
+      next if (!($pid =~ /^\d+$/));
+      push @pids, $pid;
+    }
   }
 }
 
